a059ea8b09149d98e8717cabc853167d84888c47
Lucas HDP <lucashdp@outlook.com>
2024-03-27 19:45:17 -0300

    fix: Add config env module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from './modules/config-env/config-env.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { ThrottlerModule } from './modules/throttler/throttler.module';
 import { JwtModule } from './modules/jwt/jwt.module';
 
 @Module({
-  imports: [DatabaseModule, JwtModule, ThrottlerModule],
+  imports: [ConfigModule, DatabaseModule, JwtModule, ThrottlerModule],
   controllers: [AppController],
   providers: [AppService],
 })
