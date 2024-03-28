3c25d7a69f7ce38ee445a55a6daddb637554fdea
Lucas HDP <lucashdp@outlook.com>
2024-03-27 21:36:39 -0300

    fix: Fix database module

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: 3306,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
