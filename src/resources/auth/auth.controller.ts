b6aee025e768760d704d5c92c2a556ee064d498c
Lucas HDP <lucashdp@outlook.com>
2024-04-06 14:22:34 -0300

    chore: Add HttpStatus enum

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { ApiTags, ApiBody, ApiResponse, } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
@@ -14,21 +14,21 @@ export class AuthController {
   @ApiBody({ type: SigninDto })
   @ApiResponse({
     description: 'User logged in successfully',
-    status: 200,
+    status: HttpStatus.OK,
     type: ResultType<AuthResultDto>,
   })
   @ApiResponse({
     description: 'Bad request',
-    status: 400,
+    status: HttpStatus.BAD_REQUEST,
     type: ResultType,
   })
   @ApiResponse({
     description: 'Unauthorized',
-    status: 401,
+    status: HttpStatus.UNAUTHORIZED,
     type: ResultType,
   })
   @Post('signin')
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   signin(@Body() signinDto: SigninDto): Promise<ResultType<AuthResultDto>> {
     return this.authService.signIn(signinDto);
   }
@@ -36,16 +36,16 @@ export class AuthController {
   @ApiBody({ type: SignupDto })
   @ApiResponse({
     description: 'Method to sign user up',
-    status: 201,
+    status: HttpStatus.CREATED,
     type: ResultType<AuthResultDto>,
   })
   @ApiResponse({
     description: 'Bad request',
-    status: 400,
+    status: HttpStatus.BAD_REQUEST,
     type: ResultType,
   })
   @Post('signup')
-  @HttpCode(201)
+  @HttpCode(HttpStatus.CREATED)
   signup(@Body() signupDto: SignupDto): Promise<ResultType<AuthResultDto>> {
     return this.authService.signup(signupDto);
   }
