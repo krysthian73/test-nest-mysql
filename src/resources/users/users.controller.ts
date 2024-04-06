f643f66ea3d63f0e4c3cf494c2a98bfd61513e53
Lucas HDP <lucashdp@outlook.com>
2024-04-06 11:23:30 -0300

    refactor: Remove exposed create user from users controller

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -1,24 +1,19 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@ApiTags('users')
+@Controller('api/v1/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   findAll() {
