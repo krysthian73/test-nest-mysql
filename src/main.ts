f1f2261f9a02c39e43494edb5160a08dd7da0930
Lucas HDP <lucashdp@outlook.com>
2024-04-06 11:22:49 -0300

    chore: Add class transformer and bcrypt

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,10 +10,10 @@ async function bootstrap() {
     .setTitle('API docs')
     .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
