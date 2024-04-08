9ee5749d067c28233f24053730d1a4143fd048f7
Lucas HDP <lucashdp@outlook.com>
2024-04-07 23:46:45 -0300

    feat: Implements endpoint to return user tasks

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -15,11 +15,16 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserFullDto } from './dto/user-full.dto';
 import { AuthOwnerAdminGuard } from '../auth-owner-admin.guard';
 import { ResultType } from '../types';
+import { TaskFullDto } from '../tasks/dto/task-full.dto';
+import { TasksService } from '../tasks/tasks.service';
 
 @ApiTags('users')
 @Controller('api/v1/users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly tasksService: TasksService,
+  ) {}
 
   @Get()
   @UseGuards(AuthOwnerAdminGuard)
@@ -127,5 +132,29 @@ export class UsersController {
   async remove(@Param('id') id: string): Promise<ResultType<null>> {
     await this.usersService.remove(+id);
     return new ResultType(HttpStatus.OK, [], 'User deleted successfully');
+  }
+
+  @Get(':id/tasks')
+  @UseGuards(AuthOwnerAdminGuard)
+  @ApiBearerAuth()
+  @ApiResponse({
+    description: 'User tasks found successfully',
+    status: HttpStatus.OK,
+    type: ResultType<TaskFullDto[]>,
+  })
+  @ApiResponse({
+    description: 'Unauthorized',
+    status: HttpStatus.UNAUTHORIZED,
+    type: ResultType,
+  })
+  @HttpCode(HttpStatus.OK)
+  async findAllTasks(
+    @Param('id') id: string,
+  ): Promise<ResultType<TaskFullDto[]>> {
+    const tasks = await this.tasksService.findAll({ userId: +id });
+    const tasksFullDto: TaskFullDto[] = tasks.map(
+      (task) => new TaskFullDto(task),
+    );
+    return new ResultType<TaskFullDto[]>(HttpStatus.OK, [], '', tasksFullDto);
   }
 }
