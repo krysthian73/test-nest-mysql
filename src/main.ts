d5e0afc472b98495d1f2f770f46644b5b5000ac9
Krysthian Simoes <krysthiansimoes@Krysthians-MacBook-Air.local>
2024-09-24 07:43:00 -0300

    improvement: adding enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('API docs')
